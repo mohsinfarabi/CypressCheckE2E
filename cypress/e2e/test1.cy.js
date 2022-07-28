034924b6b98462391f446c197c09d1cc67d9ad65
mohsinfarabi <mohsin.farabi@gmail.com>
2022-07-28 18:34:19 +0600

    second commit

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,7 +1,7 @@
 it('Google Search', () => {
     cy.visit('https://www.google.com')
 
-    cy.get('.gLFyf').type('Automation step by step{enter}')
+    cy.get('.gLFyf', {timeout: 5000}).type('Automation step by step{enter}')
 
     //cy.contains('Goggle Search').click()
 });
