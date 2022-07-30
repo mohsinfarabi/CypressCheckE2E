f1ba68777f530d74f301fa9c90546618801fb2e3
mohsinfarabi <mohsin.farabi@gmail.com>
2022-07-31 00:58:11 +0600

    new another one

--- cypress/e2e/selectlocation.cy.js
+++ cypress/e2e/selectlocation.cy.js
@@ -8,16 +8,6 @@ describe('Should Select Test Area Delivery Location',()=>{
         cy.visit('https://www.bengalmeat.com') 
     })
     it('Select Test Area as Delivery Location', () => {
-        
-        // cy.get(':nth-child(1) > .form-group > .css-b62m3t-container > .css-1s2u09g-control > .css-1d8n9bt > .css-ackcql').click({force:true})
-        // cy.get('#react-select-2-option-0').click({force:true})
-        // cy.get(':nth-child(2) > .form-group > .css-b62m3t-container > .css-1s2u09g-control > .css-1d8n9bt > .css-ackcql').click({force:true})
-        // //cy.get(':nth-child(2) > .form-group > .css-b62m3t-container > .css-1s2u09g-control > .css-1d8n9bt > .css-ackcql').type('Test Area',{force:true}).type('{enter}')
-        // cy.get('#react-select-3-option-76').click({force:true})
-        // //cy.get('.highlight').click({force:true})
-        // cy.wait(5000)
-        // cy.contains('Confirm').click({force:true})
-        //cy.log(cy.contains('Test Area'))
 
         landingPage.selectCity()
         landingPage.selectDhaka()
